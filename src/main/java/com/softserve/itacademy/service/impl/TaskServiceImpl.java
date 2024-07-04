1103c2ff8945cb38745f3e8cc9d8c9261fc6a276
VasylyshynDmytro <147658570+VasylyshynDmytro@users.noreply.github.com>
2024-07-04 21:44:09 +0300

    Update TaskServiceImpl.java

--- src/main/java/com/softserve/itacademy/service/impl/TaskServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/TaskServiceImpl.java
@@ -1,5 +1,7 @@
 package com.softserve.itacademy.service.impl;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
+import com.softserve.itacademy.exception.EntityNotFoundException;
 import com.softserve.itacademy.model.Task;
 import com.softserve.itacademy.repository.TaskRepository;
 import com.softserve.itacademy.service.TaskService;
@@ -11,33 +13,49 @@ import java.util.Optional;
 
 @Service
 public class TaskServiceImpl implements TaskService {
-    private TaskRepository taskRepository;
+    private final TaskRepository taskRepository;
 
     public TaskServiceImpl(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
     @Override
-    public Task create(Task user) {
-            return taskRepository.save(user);
+    public Task create(Task task) {
+        if (task == null) {
+            throw new NullEntityReferenceException("Task can not be null");
+        }
+        return taskRepository.save(task);
     }
 
     @Override
     public Task readById(long id) {
         Optional<Task> optional = taskRepository.findById(id);
+        if (optional.isPresent()) {
             return optional.get();
+        } else {
+            throw new EntityNotFoundException("task - " + id + " not found");
+        }
     }
 
     @Override
     public Task update(Task task) {
-            Task oldTask = readById(task.getId());
-                return taskRepository.save(task);
+        if (task == null) {
+            throw new NullEntityReferenceException("task can not be null");
+        }
+        Task oldTask = readById(task.getId());
+        if (oldTask == null) {
+            throw new EntityNotFoundException("task - " + task.getId() + " not found");
+        }
+        return taskRepository.save(task);
     }
 
     @Override
     public void delete(long id) {
         Task task = readById(id);
-            taskRepository.delete(task);
+        if (task == null) {
+            throw new EntityNotFoundException("task - " + id + " not found");
+        }
+        taskRepository.delete(task);
     }
 
     @Override
