aec72e5903146ca028ea73b651f515a5bfb0781d
VasylyshynDmytro <147658570+VasylyshynDmytro@users.noreply.github.com>
2024-07-04 21:43:38 +0300

    Update RoleServiceImpl.java

--- src/main/java/com/softserve/itacademy/service/impl/RoleServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/RoleServiceImpl.java
@@ -1,5 +1,7 @@
 package com.softserve.itacademy.service.impl;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
+import com.softserve.itacademy.exception.EntityNotFoundException;
 import com.softserve.itacademy.model.Role;
 import com.softserve.itacademy.repository.RoleRepository;
 import com.softserve.itacademy.service.RoleService;
@@ -12,7 +14,7 @@ import java.util.Optional;
 @Service
 public class RoleServiceImpl implements RoleService {
 
-    private RoleRepository roleRepository;
+    private final RoleRepository roleRepository;
 
     public RoleServiceImpl(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
@@ -20,25 +22,41 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public Role create(Role role) {
-            return roleRepository.save(role);
+        if (role == null) {
+            throw new NullEntityReferenceException("role can not be null");
+        }
+        return roleRepository.save(role);
     }
 
     @Override
     public Role readById(long id) {
         Optional<Role> optional = roleRepository.findById(id);
+        if (optional.isPresent()) {
             return optional.get();
+        } else {
+            throw new EntityNotFoundException("role - " + id + " not found");
+        }
     }
 
     @Override
     public Role update(Role role) {
-            Role oldRole = readById(role.getId());
-                return roleRepository.save(role);
+        if (role == null) {
+            throw new NullEntityReferenceException("role can not be null");
+        }
+        Role oldRole = readById(role.getId());
+        if (oldRole == null) {
+            throw new EntityNotFoundException("role - " + role.getId() + " not found");
+        }
+        return roleRepository.save(role);
     }
 
     @Override
     public void delete(long id) {
         Role role = readById(id);
-            roleRepository.delete(role);
+        if (role == null) {
+            throw new EntityNotFoundException("role - " + id + " not found");
+        }
+        roleRepository.delete(role);
     }
 
     @Override
