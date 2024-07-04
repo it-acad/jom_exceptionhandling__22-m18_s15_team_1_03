4639d6dfb15fd19e37356d7fb4835f3a14411896
VasylyshynDmytro <147658570+VasylyshynDmytro@users.noreply.github.com>
2024-07-04 21:44:24 +0300

    Update ToDoServiceImpl.java

--- src/main/java/com/softserve/itacademy/service/impl/ToDoServiceImpl.java
+++ src/main/java/com/softserve/itacademy/service/impl/ToDoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.softserve.itacademy.service.impl;
 
+import com.softserve.itacademy.exception.NullEntityReferenceException;
+import com.softserve.itacademy.exception.EntityNotFoundException;
 import com.softserve.itacademy.model.ToDo;
 import com.softserve.itacademy.repository.ToDoRepository;
 import com.softserve.itacademy.service.ToDoService;
@@ -20,25 +22,41 @@ public class ToDoServiceImpl implements ToDoService {
 
     @Override
     public ToDo create(ToDo todo) {
-            return todoRepository.save(todo);
+        if (todo == null) {
+            throw new NullEntityReferenceException("toDo can not be null");
+        }
+        return todoRepository.save(todo);
     }
 
     @Override
     public ToDo readById(long id) {
         Optional<ToDo> optional = todoRepository.findById(id);
+        if (optional.isPresent()) {
             return optional.get();
+        } else {
+            throw new EntityNotFoundException("toDo - " + id + " not found");
+        }
     }
 
     @Override
     public ToDo update(ToDo todo) {
-            ToDo oldTodo = readById(todo.getId());
-                return todoRepository.save(todo);
+        if (todo == null) {
+            throw new NullEntityReferenceException("toDo can not be null");
+        }
+        ToDo oldTodo = readById(todo.getId());
+        if (oldTodo == null) {
+            throw new EntityNotFoundException("toDo - " + todo.getId() + " not found");
+        }
+        return todoRepository.save(todo);
     }
 
     @Override
     public void delete(long id) {
         ToDo todo = readById(id);
-            todoRepository.delete(todo);
+        if (todo == null) {
+            throw new EntityNotFoundException("toDo - " + id + " not found");
+        }
+        todoRepository.delete(todo);
     }
 
     @Override
